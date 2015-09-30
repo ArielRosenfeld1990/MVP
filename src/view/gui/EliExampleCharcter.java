e63f4eef1d82c5dd21e79f733c1798f273039251
ofir <ofir@ofir-PC>
2015-09-30 14:54:50 +0300

    new character

--- src/view/gui/EliExampleCharcter.java
+++ src/view/gui/EliExampleCharcter.java
@@ -19,7 +19,9 @@ public class EliExampleCharcter extends MazeCharacterGuiDisplayer {
 //		paintEvent.gc.setBackground(new Color(null,0,0,0));
 		BufferedInputStream character;
 		try {
-			character = new BufferedInputStream(new FileInputStream("lib/minonCharacter.png"));
+			//character = new BufferedInputStream(new FileInputStream("lib/minonCharacter.png"));
+			character = new BufferedInputStream(new FileInputStream("lib/seaEmperor.png"));
+			//character = new BufferedInputStream(new FileInputStream("lib/hackchar.png"));
 			Image im = new Image(null, character);
 			paintEvent.gc.drawImage(im, 0, 0, im.getImageData().width, im.getImageData().height, x, y, characterWidth, characterHeight);
 		} catch (FileNotFoundException e) {
